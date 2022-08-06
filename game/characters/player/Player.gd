f38880a49047aeea9ff55a3f1fd13701688ca56f
Andrew Lee <alee14498@protonmail.com>
2022-08-05 22:26:17 -0400

    New logo, pause, made more controls

--- game/characters/player/Player.gd
+++ game/characters/player/Player.gd
@@ -5,7 +5,7 @@ export var GRAVITY = 20
 export var MAXFALLSPEED = 200
 export var MAXSPEED = 80
 export var JUMPFORCE = 300
-export var ACCEL = 10
+export var ACCEL = 20
 
 var motion = Vector2()
 
@@ -20,15 +20,23 @@ func _physics_process(_delta):
 	
 	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED
-	
-	if is_on_floor():
-		if Input.is_action_just_released("jump"):
-			motion.y = -JUMPFORCE
-			pass
-			
+		
 	motion.x = clamp(motion.x,-MAXSPEED,MAXSPEED)
 		
-	motion.x += ACCEL
+	if Input.is_action_pressed("left"):
+		motion.x -= ACCEL
+		
+	elif Input.is_action_pressed("right"):
+		motion.x += ACCEL
+	else:
+		motion.x = lerp(motion.x,0,0.2)
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			motion.y = -JUMPFORCE
+			pass
+		
+	
 	motion = move_and_slide(motion,UP)
 	
 	pass
