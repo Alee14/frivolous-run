9541a472e0b38369d169ad3f7bb250a8127bc471
Andrew Lee <alee14498@protonmail.com>
2022-08-12 22:58:33 -0400

    Added new music; Victor

--- game/Pause.gd
+++ game/Pause.gd
@@ -12,6 +12,11 @@ func _on_Resume_pressed():
 	get_tree().paused = new_pause_state
 	visible = new_pause_state
 
+func _on_Mute_pressed():
+	var player = get_node("../../AudioStreamPlayer")
+	var player_paused = not player.stream_paused
+	player.stream_paused = player_paused
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
---
+++ game/characters/victor/Victor.gd
@@ -0,0 +1,7 @@
+extends KinematicBody2D
+
+var speed = 100
+var velocity = Vector2(0, 0)
+
+func _physics_process(_delta):
+	pass
